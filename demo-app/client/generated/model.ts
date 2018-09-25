1d6450b5eede2884d2709e2c9f62fb1de254a441
keisuke oohashi <keisuke.oohashi@gmail.com>
2018-09-25 14:19:16 +0900

    Export type alias when def is type alias

--- demo-app/client/generated/model.ts
+++ demo-app/client/generated/model.ts
@@ -19,5 +19,7 @@ export * from './defs/MapStructure'; // sources: mapStructure
 export * from './defs/MapContent'; // sources: mapContent
 export * from './defs/MapObject'; // sources: mapObject
 export * from './defs/RefStatus';
+export * from './defs/Alias';
+export * from './defs/UseAlias';
 export * from './defs/GetGoodsListGeneratedInlineModel';
 export * from './defs/ArrayGeneratedInlineModel';
